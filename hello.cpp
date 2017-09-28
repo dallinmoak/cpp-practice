3a0a0ebf33432a37c065082c0dd338557d97af5b
Dallin Moak <dallinmoak@gmail.com>
2017-09-27 23:23:01 -0600

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
 //this is a comment that I am adding
 int main() {
-   cout << "Hello, world!\n\n\n" << endl;
+   cout << "Hello, world!" << endl;
+   cout << "What is your name?" endl;
+   string name;
+   getline(cin,name);
+   int size = name.size();
    return 0;
 }
