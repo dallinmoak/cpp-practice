42b3da9fc002a4b6d20955665f70a4049f7b96b4
Dallin Moak <dallinmoak@gmail.com>
2017-09-27 22:53:35 -0600

    Merge branch 'master' of https://github.com/dallinmoak/cpp-practice

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
 using namespace std;
-<<<<<<< HEAD
 
 int main() {
    cout << "Hello, world!\n\n\n" << endl;
    return 0;
 }
-=======
- 
-int main() {
-   cout << "Hello, world!" << endl;
-   return 0;
-}
->>>>>>> 7e018879ad9443b975a43bcac27b7e7decff9aff
