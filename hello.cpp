946f2373f70a3447681a4332f905a96117f261dc
Dallin Moak <dallinmoak@gmail.com>
2017-09-28 00:15:32 -0600

    idk

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,6 @@ string goodbyemsg();
 
 //this is a comment that I am adding
 int main() {
-<<<<<<< HEAD
    cout << "Hello, world!\n\n";
    cout << "What is your name? ";
    string name;
@@ -15,13 +14,6 @@ int main() {
    cout << "I see you have " << namesize << "* characters in your name.\n"
     << "*including whitespace\n\n";
    cout << goodbyemsg();
-=======
-   cout << "Hello, world!" << endl;
-   cout << "What is your name?" endl;
-   string name;
-   getline(cin,name);
-   int size = name.size();
->>>>>>> 3a0a0ebf33432a37c065082c0dd338557d97af5b
    return 0;
 }
 
