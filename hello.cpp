15cca8122660f1dbe37633618f493a3d4961be51
Dallin Moak <dallinmoak@gmail.com>
2017-09-27 23:23:01 -0600

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <array>
 using namespace std;
+
+string goodbyemsg();
 
 //this is a comment that I am adding
 int main() {
-   cout << "Hello, world!\n\n\n" << endl;
+   cout << "Hello, world!\n\n";
+   cout << "What is your name? ";
+   string name;
+   getline(cin,name);
+   int namesize = name.size();
+   cout << "I see you have " << namesize << "* characters in your name.\n"
+    << "*including whitespace\n\n";
+   cout << goodbyemsg();
    return 0;
+}
+
+string goodbyemsg(){
+  return "this is all...\n\n\n\n...goodbye now...\n";
 }
