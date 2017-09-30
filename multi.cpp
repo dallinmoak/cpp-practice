1c52000af3c8551b676b8a15b79315f5a5f1e394
Dallin Moak <dallinmoak@gmail.com>
2017-09-30 17:36:07 -0600

    update

--- multi.cpp
+++ multi.cpp
@@ -2,12 +2,20 @@
 
 using namespace std;
 
-void printRect(int height, int width);
+void printRect(int height, int width, int bheight, int bwidth);
+void getRecSize(string choice);
 
 int main(){
-  int height = 3;
-  int width = 3;
-  printRect(height,width);
+  cout << "what kind of rectangle would you like?\n";
+  cout << "for one built of SQUARES, press 's'\n";
+  cout << "for one made of adjustible RECTANGLES, press 'r' ";
+  string choice;
+  cin >> choice;
+  getRecSize(choice);
+  cout << "\nPress any key, then RETURN to exit ";
+  int junk;
+  cin >> junk;
+
 }
 /*
  _ _
@@ -23,41 +31,54 @@ this one is 20/3 = 6 r2
 a box is 4 characters wide and 2 lines tall.
 
 */
+void getRecSize(string choice){
+  int height;
+  int width;
+  int bheight;
+  int bwidth;
+  cout << "height of rectange? ";
+  cin >> height;
+  cout << "width of rectange? ";
+  cin >> width;
+  if (choice != "r"){
+    cout << "size of unit sqares? ";
+    cin >> bheight;
+    bwidth = bheight;
+  }
+  else{
+    cout << "height of unit squares? ";
+    cin >> bheight;
+    cout << "width of unit square? ";
+    cin >> bwidth;
+  }
+  printRect(height,width, bheight, bwidth);
+}
 
-void printRect(int height, int width){
-  int bheight = 2;
-  int bwidth = 2;
-  for (int row =0; row <bheight*height; row++){
+void printRect(int height, int width, int bheight, int bwidth){
+  for (int row =0; row <=(bheight*height); row++){
     //first row
     if (row == 0){
       for (int col = 0; col < (bwidth*width); col++)cout << " _";
-      cout << endl;
+      cout << " \n";
     }
     //middle rows
     else if (row%bheight !=0){
       for (int col = 0; col < (bwidth*width); col++){
         //edgle columns
-        if (col%bwidth == 0){
-          //last edgle
-          if (col+1 == (bwidth*width))cout << "|\n";
-          //all other edges
-          else cout << "| ";
-        }
+        if (col%bwidth == 0) cout << "| ";
         //middle columns
         else cout << "  ";
       }
+      cout << "|\n";
     }
     //bottom rows
     else{
       for (int col = 0; col < (bwidth*width); col++){
-        if (col%bwidth ==0){
-          if(col+1 ==bwidth* width)cout << "|\n";
-          else cout << "|_";
-        }
-        else if (col%bwidth !=0){
-          cout << " _";
-        }
+        //edgle columns
+        if (col%bwidth ==0)cout << "|_";
+        else cout << " _";
       }
+      cout << "|\n";
     }
   }
 
