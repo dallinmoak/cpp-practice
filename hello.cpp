5bad7636793b22f281e8257f9c839cc5fd1b69b4
Dallin Moak <dallinmoak@gmail.com>
2017-10-02 18:38:04 -0600

    practicing control flow and strings
    I now want hello.cpp to print out the american flag. struggling with runtime error.

--- hello.cpp
+++ hello.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
-#include <array>
+#include <string>
+
 using namespace std;
 
-string goodbyemsg();
+void printStars(int row, int col);
+string topStripes(int r);
+void printStripes(int row, int col);
 
-//this is a comment that I am adding
+
 int main() {
-   cout << "Hello, world!\n\n";
-   cout << "What is your name? ";
-   string name;
-   getline(cin,name);
-   int namesize = name.size();
-   cout << "I see you have " << namesize << "* characters in your name.\n"
-    << "*including whitespace\n\n";
-   cout << goodbyemsg();
-   return 0;
+  int rows= 5;
+  int cols = 10;
+  printStars(rows, cols);
+  printStripes(rows, cols);
+  return 0;
 }
 
-string goodbyemsg(){
-  return "this is all...\n\n\n\n...goodbye now...\n";
+void printStars(int row, int col){
+  for (int i=0; i <(col-1);i++){
+    for (int j=0; j <row;j++){
+      if (i%2==0) cout << "*    ";
+      else if (j==0)cout << "   *";
+      else cout << "    *";
+    }
+    if (i%2 ==0) cout << "*\n"<< topStripes(row);
+    else cout << endl;
+  }
+}
+
+string topStripes(int r){
+  char str[] = "";
+  for (int i = 0;i<(3*r);i++){
+    strcat (str, "____");
+  }
+  return str;
+}
+
+void printStripes(int row, int col){
+  for (int i = 0; i < row; i++){
+    for (int j=0; j < (3*col); j++){
+      cout << "____";
+    }
+    cout << endl;
+  }
 }
