ee0c93449daceaf02dc1301d4ffcbb0ac8f21042
dallinmoak <dallinmoak@gmail.com>
2017-10-05 13:22:01 -0600

    now the squares are sent to a stringstream and then it's printed to cout

--- div.cpp
+++ div.cpp
@@ -1,47 +1,55 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
-void printbSize(int nBlocks, int bSize);
-void printRows(int num, int bSize);
+void printbSize(int nBlocks);
+void printRows(int num, bool first);
+
+stringstream ss;
 
 int main(){
   int num;
-  int size = 2;
   cout << "num? ";
   cin >> num;
-  printbSize(num, size);
+  printbSize(num);
+  cout << ss.str();
   return 0;
 }
 
-void printbSize(int nBlocks, int bSize){
+void printbSize(int nBlocks){
   int max =20;
   int rows = nBlocks/max;
   int remain= nBlocks%max;
   for (int rowCount = 0; rowCount < rows; rowCount++){
-    printRows(max,bSize);
+    if (rowCount==0){
+      printRows(max,true);
+    }
+    else printRows(max,false);
   }
-  printRows(remain,bSize);
+  if (remain >0) printRows(remain,false);
 }
 
-void printRows(int num, int bSize){
-  for (int i=0; i < num*bSize; i++){
-    cout << " _";
+void printRows(int num, bool first){
+  int bSize=2;
+  if (first){
+    for (int i=0; i < num*bSize; i++){
+      ss << " _";
+    }
+    ss << endl;
   }
-  cout << endl;
-  for (int i=0; i < num*bSize; i++){
-    if(i%bSize==0) cout << "| ";
-    else cout << "  ";
-  }
-  cout << "|\n";
-  for (int i=0; i < num*bSize; i++){
-    if(i%bSize==0) cout << "|_";
-    else cout << " _";
-  }
-  cout << "|\n";
-}
 
-/*
- _ _ _ _ _ _
- _ _ _|_ _ _|
- */
+  for (int i=0; i < num*bSize; i++){
+    if(i%bSize==0) ss << "| ";
+    else ss << "  ";
+  }
+  ss << "|\n";
+  int count =0;
+  for (int i=0; i <(num*bSize); i++){
+    if(i%bSize==0) ss << "|_";
+    else ss << " _";
+    count++;
+  }
+  ss << "|\n";
+}
