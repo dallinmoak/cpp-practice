a03d87ccb502e44d7dee7de41c8802a82b3000de
dallinmoak <dallinmoak@gmail.com>
2017-10-14 23:49:15 -0600

    th

--- appointment.h
+++ appointment.h
@@ -9,17 +9,10 @@ class Appointment{
 public:
   Appointment();
   Appointment(string n);
-<<<<<<< HEAD
   int getTime() const;
   string getName() const;
   void setTime(int t);
   void setName(string n);
-=======
-  int getTime();
-  string getName();
-  void setTime(int t) const;
-  void setName(string n) const;
->>>>>>> 47fa5b6689ae9f737bfd27af7f0ac46d2025c7a2
 private:
   string name;
   int timex;
