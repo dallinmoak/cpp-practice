c9bfb51abac07b4a887c754985ea24213d25a33a
dallinmoak <dallinmoak@gmail.com>
2017-10-14 23:46:11 -0600

    t

--- appointment.cpp
+++ appointment.cpp
@@ -18,12 +18,19 @@ Appointment::Appointment(string n){
 
 }
 
+<<<<<<< HEAD
 int Appointment::getTime() const{
   return timex;
 }
 
 string Appointment::getName() const{
+=======
+int getTime() const{
+  return timex;
+}
 
+string getName()const{
+>>>>>>> 47fa5b6689ae9f737bfd27af7f0ac46d2025c7a2
   return name;
 }
 
--- appointment.h
+++ appointment.h
@@ -2,16 +2,24 @@
 #define APPOINTMENT_H
 #include <string>
 
+
 using namespace std;
 
 class Appointment{
 public:
   Appointment();
   Appointment(string n);
+<<<<<<< HEAD
   int getTime() const;
   string getName() const;
   void setTime(int t);
   void setName(string n);
+=======
+  int getTime();
+  string getName();
+  void setTime(int t) const;
+  void setName(string n) const;
+>>>>>>> 47fa5b6689ae9f737bfd27af7f0ac46d2025c7a2
 private:
   string name;
   int timex;
